1316991f7757dcf77a74145b3008feae82e9ce99
Maksym Duhinov <maximakyla@gmail.com>
2025-03-04 03:20:35 +0200

    HomeWork-8.1 Create new method print

--- src/com/gmail/maximakyla/homework_8_1/ArraySearchAndSort.java
+++ src/com/gmail/maximakyla/homework_8_1/ArraySearchAndSort.java
@@ -1,7 +1,62 @@
 package com.gmail.maximakyla.homework_8_1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class ArraySearchAndSort {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input array length value");
+        int arrayLength = scanner.nextInt();
+        int[] array = new int[arrayLength];
+        Random random = new Random();
 
+        int randomMinimum = 1;
+        int randomMaximum = 100;
+        for (int i = 0; i < array.length; i++) {
+            int randomNumber = random.nextInt(randomMaximum - randomMinimum + 1) + randomMinimum;
+            array[i] = randomNumber;
+        }
+
+//        if (array.length == 0) {
+//            System.out.print("Empty array created");
+//        }
+
+        print(array);
+        scanner.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static void print(int[] array) {
+        if (array.length == 0) {
+            System.out.print("Empty array created");
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                System.out.print("Array elements: [ ");
+            }
+            if (i == array.length - 1) {
+                System.out.printf("%d ", array[i]);
+                System.out.println("]");
+            } else {
+                System.out.printf("%d, ", array[i]);
+            }
+        }
     }
 }
